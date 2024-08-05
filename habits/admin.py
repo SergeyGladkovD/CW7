dc6382400d21adfc79746c1919974da46e52962a
melcor <melcor2006@mail.ru>
2024-08-05 19:33:25 +0300

    hz

---
+++ habits/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from habits.models import Habit
+
+
+@admin.register(Habit)
+class HabitAdmin(admin.ModelAdmin):
+    list_display = ("user", "action", "is_published",)
+    list_filter = ("user",)
+    search_fields = ("action",)
